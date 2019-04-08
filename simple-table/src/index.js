edc446604a1ffce4b4be1ebdc806db7b924b9bf2
charliemedina <charliemedina1989@gmail.com>
2019-04-08 19:00:04 +0200

    Build A Static Version

--- simple-table/src/index.js
+++ simple-table/src/index.js
@@ -4,9 +4,22 @@ import './index.css';
 import FilterableProductTable from './FilterableProductTable';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<FilterableProductTable />, document.getElementById('root'));
-
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+const PRODUCTS = [
+    {category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'},
+    {category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball'},
+    {category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'},
+    {category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch'},
+    {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
+    {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'}
+  ];
+   
+  ReactDOM.render(
+    <FilterableProductTable products={PRODUCTS} />,
+    document.getElementById('root')
+  );
+
+  serviceWorker.unregister();
