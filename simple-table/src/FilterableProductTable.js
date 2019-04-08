747bd8f8dfe376cac702ce0f4923b275d37abbf7
charliemedina <charliemedina1989@gmail.com>
2019-04-08 18:27:44 +0200

    Create and clean scaffolder

---
+++ simple-table/src/FilterableProductTable.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class FilterableProductTable extends Component {
+  render() {
+    return (
+      <div className="App">
+      </div>
+    );
+  }
+}
+
+export default FilterableProductTable;
