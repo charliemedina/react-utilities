bc9f91903a4b740f58a8d7b2281d300430a1071e
charliemedina <charliemedina1989@gmail.com>
2019-04-09 13:56:51 +0200

    Adding Lifecycle Methods to a Class

--- simple-table/src/components/clock/Clock.js
+++ simple-table/src/components/clock/Clock.js
@@ -8,6 +8,23 @@ class Clock extends Component {
     this.state = {date: new Date()};
   }
 
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
   render() {
     return(
       <div>
